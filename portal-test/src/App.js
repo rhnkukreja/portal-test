eb532b9e519177c69b7f7a078bf8c06a4432fd7e
Rohan Kukreja <rhnkukreja@gmail.com>
2026-04-17 13:42:11 +0530

    Added PS's Test URL

--- portal-test/src/App.js
+++ portal-test/src/App.js
@@ -3,8 +3,7 @@ function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       <iframe
-        src="https://boundaries-officers-reaching-alex.trycloudflare.com/"
-        // src="http://localhost:3000/"
+        src="https://test-dashboard.paisasmart.com/"
 
         width="100%"
         height="100%"

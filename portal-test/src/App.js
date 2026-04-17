0bbd785d44ccd6cba167446da379b3b9b6b9c15c
Rohan Kukreja <rhnkukreja@gmail.com>
2026-04-17 13:01:27 +0530

    Added actual link

--- portal-test/src/App.js
+++ portal-test/src/App.js
@@ -2,8 +2,8 @@ function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       <iframe
-        // src="https://test-dashboard.paisasmart.com/"
-        src="http://localhost:3000/"
+        src="https://test-dashboard.paisasmart.com/"
+        // src="http://localhost:3000/"
 
         width="100%"
         height="100%"
